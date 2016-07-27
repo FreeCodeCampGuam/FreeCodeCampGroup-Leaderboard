6ab15f97dfc5c0bb57a49fb2234de879b68fe784
Gabriel Certeza <gabrielscerteza@gmail.com>
2016-07-27 21:02:06 +1000

    Built leaderboard

--- js/main.js
+++ js/main.js
@@ -27,17 +27,13 @@ function showLeaderboard() {
   var hideOthers = function() {
     $('.about').hide();
     $('.contact').hide();
-    $('.leaderboard').hide();
+    $('.home').hide();
   }();
 }
-// for testing purposes...
-// currently testing leaderboard page
-$(document).ready(function() {
-  $('.leaderboard').show();
-  $('.about').hide();
-  $('.home').hide();
-  $('.contact').hide();
+var arrayOfUsernames = [];
+function apiRequests() {
   $.ajax({
+    async: false,
     url: 'https://api.gitter.im/v1/rooms/56f9df0785d51f252abb4f57/users',
     headers: {
       'Content-Type': 'application/json',
@@ -46,36 +42,48 @@ $(document).ready(function() {
     },
     method: 'GET',
     dataType: 'json',
-    success: function(data) {
-      // console.log(data);
-      var arrayOfUsernames = [];
-      data.forEach(function(object) {
-        arrayOfUsernames.push(object.username);
-      });
-      arrayOfUsernames = arrayOfUsernames.map(function(username) {
-        
+    success: function(gitterData) {
+      // console.log(gitterData);
+      gitterData.forEach(function(object) {
+        $.ajax({
+          url: 'https://www.freecodecamp.com/api/users/about?username=' + object.username,
+          async: false,
+          method: 'GET',
+          dataType: 'json',
+          success: function(fccData) {
+            arrayOfUsernames.push([fccData.about.username, fccData.about.browniePoints]);
+          },
+          error: function() {
+            console.log(object.username + ' does not have an FCC account linked to GitHub.');
+          }
+        });
       });
     },
     error: function() {
-      console.log('failed');
+      console.log('Gitter API failed.');
     }
   });
+}
+$(document).ready(function() {
+  apiRequests();
+  arrayOfUsernames.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+  arrayOfUsernames.forEach(function(user) {
+    console.log('hi');
+    $('.leaderboard > .container').append('<div class="row"><h3>User: ' + user[0] + '</h3><h4>Brownie Points: ' + user[1] + '</div>');
+  });
+  showHome();
+  $('#about').on('click', function() {
+    showAbout();
+  });
+  $('#contact').on('click', function() {
+    showContact();
+  });
+  $('#home').on('click', function() {
+    showHome();
+  });
+  $('#leaderboard').on('click', function() {
+    showLeaderboard();
+  });
 });
-
-// $(document).ready(function() {
-
-// // need to initially hide everything except home page
-//   showHome();
-//   $('#about').on('click', function() {
-//     showAbout();
-//   });
-//   $('#contact').on('click', function() {
-//     showContact();
-//   });
-//   $('#home').on('click', function() {
-//     showHome();
-//   });
-//   $('#leaderboard').on('click', function() {
-//     showLeaderboard();
-//   });
-// });
