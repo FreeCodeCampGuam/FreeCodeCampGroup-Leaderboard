1392c636b3c4f7bd0f52c69d19339e42be6ad294
irdumb <go2knox@hotmail.com>
2016-07-29 11:06:41 +1000

    async and reorganize
    prepare for filtering options

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ function apiRequests() {
       gitterData.forEach(function(object, i) {
         var camper = {
           "display" : object.displayName,
-          "username" : "--loading--",
+          "username" : object.username,
           "points" : -1,
           "created" : null
         };
@@ -56,26 +56,27 @@ function apiRequests() {
         _displayCamper(i+1,camper);
         $.ajax({
           url: 'https://www.freecodecamp.com/api/users/about?username=' + object.username.toLowerCase(),
-          async: false,
+          async: true,
           method: 'GET',
           dataType: 'json',
           success: function(fccData) {
-            updateCamper({"username" : fccData.about.username, "points" : fccData.about.browniePoints})
+            updateCamper({"username" : object.username, "points" : fccData.about.browniePoints})  // removed about.username cause we need to find users later by unique value
+            // not sure if displayName is unique. object.username should be though. if we use object and about.username, might be differences
             //camper.concat([fccData.about.username, fccData.about.browniePoints]);
           },
           error: function() {
-            updateCamper({"username" : object.username, "points" : null})
+            updateCamper({"username" : object.username, "points" : -2})  // shrug*
             //camper.concat([object.username, null]);
           }
         });
         // Github call to get 'join' date. assuming joined github at same time
         $.ajax({
           url: 'https://api.github.com/users/' + object.username,
-          async: false,
+          async: true,
           method: 'GET',
           dataType: 'json',
           success: function(hubData) {
-            updateCamper({"created" : hubData.created_at})
+            updateCamper({"username" : object.username, "created" : hubData.created_at})
             //camper.push(hubData.created_at)
           },
           error: function() {
@@ -92,11 +93,30 @@ function apiRequests() {
   });
 }
 
-function updateCamper(data) {
-  //sort and redisplay
+function updateCamper(camper) {
+  for (let c of arrayOfUsernames) {  // is there a quicker find?
+    if (c.username === camper.username) {
+      for (let prop in camper) {
+        c[prop] = camper[prop]
+      }
+      break;
+    }
+  }
+  arrayOfUsernames.sort(function(a, b) {
+    return b.points - a.points;
+  });
+  arrayOfUsernames.forEach(function(user, i) {
+    _displayCamper(i+1,user);
+  });
 }
-function _displayCamper(place, data) {
-  $('#place'+place).
+function _displayCamper(place, camper) {
+  $('#place'+place).html('<h3>' + camper.display + ' @' + camper.username + '</h3>')
+  if (camper.points >= 0) {
+    $('#place'+place).append('<h4>Brownie Points: '+camper.points)
+  }
+  else {
+    $('#place'+place).append('<h4 class="error">Account not linked to freeCodeCamp')
+  }
 }
 
 $(document).ready(function() {
